d4291b01acd55eaec8ea195691db5ed92290df1b
princevg <princegracys@gmail.com>
2021-07-28 15:13:55 +0530

    Update user-card.js
    Test

--- user-card.js
+++ user-card.js
@@ -1,5 +1,7 @@
 export class UserCard extends HTMLElement{
+    console.log("Test")
     connectedCallback() {
+        console.log("Test Inside")
         this.innerHTML = `<button>Click on me!</button>`;
     }
   }
